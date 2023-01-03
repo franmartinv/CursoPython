f6f88bc9bd94d17882a1751c23e8ec376d707ca2
franmartinv <95523823+franmartinv@users.noreply.github.com>
2023-01-03 12:04:03 +0100

    Contribucion constructiva

--- PRUEBA.py
+++ PRUEBA.py
@@ -1,2 +1,17 @@
-print 
-print 
+#print 
+#print
+
+def function1():
+  name = input("Whats your name?: ")
+  surname = input("Whats your surname?: ")
+  
+  long_name = f"{name} {surname}"
+  
+  creator = francisco martin villegas
+  
+  print(f"Hi {name.title()} {surname.title()}! The creator of this Python script was {creator.title()} :-)")
+  
+def main():
+  function1()
+
+main()
